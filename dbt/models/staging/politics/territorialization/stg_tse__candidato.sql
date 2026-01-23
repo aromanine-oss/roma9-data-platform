b992da5722b2214207946d9499f22277c10e8ef0
Alex Romanine <aromanine@gmail.com>
2026-01-23 17:42:05 -0300

    feat(dim_party): add SCD2 party dimension with normalized staging

---
+++ dbt/models/staging/politics/territorialization/stg_tse__candidato.sql
@@ -0,0 +1,24 @@
+with source as (
+
+    select *
+    from {{ source('tse', 'tse_votacao_nominal_municipio_zona') }}
+
+),
+
+renamed as (
+
+    select
+        SAFE_CAST(ANO_ELEICAO as int)       as election_year,
+        SAFE_CAST(SQ_CANDIDATO as string)   as candidate_id,
+        SAFE_CAST(NM_CANDIDATO as string)   as candidate_name,
+        SAFE_CAST(NR_CANDIDATO as int)      as candidate_number,
+        SAFE_CAST(NR_PARTIDO as int)        as party_id,
+        SAFE_CAST(SG_PARTIDO as string)     as party_acr,
+        SAFE_CAST(SG_UF as string)       as state_abbreviation,
+        SAFE_CAST(CD_CARGO as int)          as office_id
+
+    from source
+)
+
+select *
+from renamed

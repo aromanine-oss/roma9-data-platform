ef40c2c076a011da77794635301d1c9bd561c446
Alex Romanine <aromanine@gmail.com>
2026-01-27 15:44:38 -0300

    feature: create dim_candidate with surrogate key

--- dbt/models/staging/politics/territorialization/stg_tse__candidato.sql
+++ dbt/models/staging/politics/territorialization/stg_tse__candidato.sql
@@ -7,16 +7,15 @@ with source as (
 
 renamed as (
 
-    select
+    select distinct
         SAFE_CAST(ANO_ELEICAO as int)       as election_year,
+        SAFE_CAST(CD_ELEICAO as int64)      as election_id,
         SAFE_CAST(SQ_CANDIDATO as string)   as candidate_id,
         SAFE_CAST(NM_CANDIDATO as string)   as candidate_name,
-        SAFE_CAST(NR_CANDIDATO as int)      as candidate_number,
+        NM_URNA_CANDIDATO                   as candidate_ballot_name,
         SAFE_CAST(NR_PARTIDO as int)        as party_id,
-        SAFE_CAST(SG_PARTIDO as string)     as party_acr,
-        SAFE_CAST(SG_UF as string)       as state_abbreviation,
+        SAFE_CAST(SG_PARTIDO as string)     as party_acr, --Existe só pra faciliar a leitura
         SAFE_CAST(CD_CARGO as int)          as office_id
-
     from source
 )
 
